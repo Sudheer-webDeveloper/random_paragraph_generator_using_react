156400a8aec847dd54336ae68feb18a74d05608b
Sudheer-webDeveloper <sudheertalaudi@gmail.com>
2023-11-08 14:50:37 +0530

    first commit

---
+++ src/components/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import RandomParagraph from './RandomParagraph'
+import './Style.css'
+
+function Index() {
+  return (
+    <div>
+        <h1 style={{textAlign:"center",margin:"10px,auto"}}>RandomParagraph Generator App Project No -7</h1>
+      <RandomParagraph />
+      <hr />
+    </div>
+  )
+}
+
+export default Index
